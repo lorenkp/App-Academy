fdad5f15ce1de115fc6f17e7368fac84bf0b95e6
Loren Peterson <lorenkp@gmail.com>
2014-04-16 11:45:01 -0700

    Wrap seed data in transaction.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,17 @@
-User.create(:name => "CJ")
-User.create(:name => "Flarnie")
-User.create(:name => "Jeff")
-User.create(:name => "Georges St. Pierre")
-User.create(:name => "Ned")
+ActiveRecord::Base.transaction do
 
-Course.create(:name => "Ruby 101", :instructor_id => 5)
-Course.create(:name => "Ruby 102", :instructor_id => 5, prereq_id: 1)
+  User.create(:name => "CJ")
+  User.create(:name => "Flarnie")
+  User.create(:name => "Jeff")
+  User.create(:name => "Georges St. Pierre")
+  User.create(:name => "Ned")
 
-Enrollment.create(:student_id => 3, :course_id => 1)
-Enrollment.create(:student_id => 4, :course_id => 1)
-Enrollment.create(:student_id => 1, :course_id => 2)
-Enrollment.create(:student_id => 2, :course_id => 2)
+  Course.create(:name => "Ruby 101", :instructor_id => 5)
+  Course.create(:name => "Ruby 102", :instructor_id => 5, prereq_id: 1)
+
+  Enrollment.create(:student_id => 3, :course_id => 1)
+  Enrollment.create(:student_id => 4, :course_id => 1)
+  Enrollment.create(:student_id => 1, :course_id => 2)
+  Enrollment.create(:student_id => 2, :course_id => 2)
+
+end
