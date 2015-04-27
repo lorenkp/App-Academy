01c3b10e98e9846d400714a0271320030661fa35
Loren Peterson <lorenkp@gmail.com>
2015-04-27 15:50:12 -0700

    Add get input to Game class. Validate input. \n Implement reveal/flag logic in tiles.

--- Minesweeper.rb
+++ Minesweeper.rb
@@ -13,17 +13,32 @@ class Tile
   def reveal
     self.revealed = true
     @public_display = (neighbor_bomb_count > 0 ? neighbor_bomb_count.to_s : "_")
+    if neighbor_bomb_count == 0
+      tiles = neighbors.map{ |position| @board.get_tile(position) }
+      tiles.each do |tile|
+        tile.reveal unless tile.flagged? || tile.revealed?
+      end
+    end
+
+    nil
   end
 
   def bombed?
     @bombed
   end
 
-  def flag
-    @flag = true
-    @public_display = "F"
+  def flagged?
+    @flagged?
   end
 
+  def revealed?
+    @revealed
+  end
+
+  def flag
+    @flag = !@flag
+    @public_display == "*" ? "F" : "*"
+  end
 
   def neighbors
     neighbors = []
@@ -52,6 +67,7 @@ class Board
     @grid_size = 9
     @bomb_num = 10
     @grid = Array.new(@grid_size) { Array.new(@grid_size) }
+    make_grid
   end
 
   def get_tile(pos)
@@ -61,8 +77,6 @@ class Board
 
   def make_grid
     @grid_size.times do |row|
-      p row
-      p @grid
       @grid_size.times do |col|
         @grid[row][col] = Tile.new([row, col])
       end
@@ -83,6 +97,8 @@ class Board
         tile.board = self
       end
     end
+
+    nil
   end
 
   def display
@@ -93,4 +109,62 @@ class Board
     nil
   end
 
+end
+
+class Game
+
+  def initialize
+    @board = Board.new
+  end
+
+  def play
+    over = false
+    until over
+      @board.display
+      input = get_input
+      tile = @board.get_tile(input[:position])
+      if choice == "f"
+        tile.flag
+      else
+        if tile.bombed?
+          p "YOU LOSE, SUCKA"
+          return
+        else
+          tile.reveal
+        end
+      end
+      
+    end
+  end
+
+  def get_input
+    puts "Choose square to reveal, i.e. f/r, row, column"
+    input = gets.chomp.split(", ")
+    choice = input.shift.downcase!
+    position = input.map(&:to_i)
+    until valid_input?(choice, position)
+      input = gets.chomp.split(", ")
+      choice = input.shift.downcase!
+      position = input.map(&:to_i)
+    end
+
+    {choice: choice, position: position}
+  end
+
+  def valid_input?(choice, position)
+    x, y = position
+    if !['f', 'r'].include?(choice)
+      puts "Please choose flag: f or reveal: r."
+    elsif !x.between?(0,8) || !y.between?(0,8)
+      puts "That is not a valid entry. Please try again."
+    elsif @board.get_tile(position).revealed?
+      puts "That tile is already revealed. Please try again."
+    elsif @board.get_tile(position).flagged? && choice == 'r'
+      puts "Can't reveal a flagged tile. Please try again"
+    else
+      return true
+    end
+
+    return false
+  end
 end
