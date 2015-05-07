9a74887627465fc538e683e77f135a00f14d7e06
Loren Peterson <lorenkp@gmail.com>
2015-05-06 17:21:08 -0700

    Finish url_shortener app

--- url_shortener/app/models/shortened_url.rb
+++ url_shortener/app/models/shortened_url.rb
@@ -23,4 +23,29 @@ class ShortenedUrl < ActiveRecord::Base
     :primary_key => :id
   )
 
+  has_many(
+    :visits,
+    :class_name => 'Visit',
+    :foreign_key => :shortened_url_id,
+    :primary_key => :id
+  )
+
+  has_many(
+    :visitors,
+    Proc.new { distinct },
+    :through => :visits,
+    :source => :visitor
+  )
+
+  def num_clicks
+    self.visits.count
+  end
+
+  def num_uniques
+    self.visitors.count
+  end
+
+  def num_recent_uniques
+    self.visits.where(["created_at BETWEEN ? AND ?", 10.minutes.ago, Time.now]).count
+  end
 end
--- url_shortener/app/models/user.rb
+++ url_shortener/app/models/user.rb
@@ -9,4 +9,18 @@ class User < ActiveRecord::Base
     :primary_key => :id
   )
 
+  has_many(
+    :visits,
+    :class_name => 'Visit',
+    :foreign_key => :user_id,
+    :primary_key => :id
+  )
+
+  has_many(
+    :visited_urls,
+    Proc.new { distinct },
+    :through => :visits,
+    :source => :visited_url
+  )
+
 end
