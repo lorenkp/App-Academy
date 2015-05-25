8979066afd7383150a1874e49b0faa36a14c841c
Loren Peterson <lorenkp@gmail.com>
2015-05-25 14:11:18 -0700

    finished curry

--- argumentsExercises.js
+++ argumentsExercises.js
@@ -29,5 +29,61 @@ var gizmo = {
   name: "gizmo"
 };
 
-var gizmoDoingSomething = doSomething.myBind(gizmo, 1,2,3);
-console.log(gizmoDoingSomething(4, 5, 6));
+// var gizmoDoingSomething = doSomething.myBind(gizmo, 1,2,3);
+//
+// console.log("gizmo function below here");
+// console.log(gizmoDoingSomething);
+//
+// console.log(gizmoDoingSomething(4, 5, 6));
+
+// curriedSum
+
+function curriedSum(numArgs) {
+  var numbers = [];
+  return function _curriedSum(n) {
+      numbers.push(n);
+      if (numbers.length === numArgs) {
+        var result = 0;
+        for(var i = 0; i < numbers.length; i++) {
+          result += numbers[i];
+        }
+        return result;
+      } else {
+        return _curriedSum;
+    }
+  };
+}
+// var sum = curriedSum(4);
+// console.log(sum(5));
+
+// sum2 = sum(5);
+// sum3 = sum2(4);
+// sum4 = sum3(4);
+// number = sum4(1);
+//
+// console.log(console.log);
+//
+// var sum = curriedSum(4);
+// var sum1 = sum(5);
+// var sum2 = sum1(30);
+
+// function#curry(numArgs)
+
+Function.prototype.curry = function(numArgs){
+  var numbers = [];
+  var funky = this;
+  return function _curry(n) {
+    numbers.push(n);
+    if (numbers.length === numArgs) {
+      console.log(numbers);
+      return funky.apply(null, numbers);
+    } else {
+      return _curry;
+    }
+  };
+};
+
+function sumThree(num1, num2, num3) {
+  return num1 + num2 + num3;
+}
+console.log(sumThree.curry(3)(4)(20)(6));
