7a0e2c9837c60305b1ce5da561540d9c8f9602b4
Loren Peterson <lorenkp@gmail.com>
2015-05-25 11:43:42 -0700

    finish myBind

--- argumentsExercises.js
+++ argumentsExercises.js
@@ -8,4 +8,26 @@ function sum() {
   return result;
 }
 
-sum(1, 2, 3, 4);
+// sum(1, 2, 3, 4);
+
+Function.prototype.myBind = function(obj) {
+  var fn = this;
+  var args1 = Array.prototype.slice.call(arguments, 1);
+  return function () {
+    var args2 = Array.prototype.slice.call(arguments);
+    return fn.apply(obj, args1.concat(args2));
+  };
+};
+
+
+function doSomething(a, b, c, d, e) {
+  console.log(this.name);
+  return a + b + c + d + e;
+}
+
+var gizmo = {
+  name: "gizmo"
+};
+
+var gizmoDoingSomething = doSomething.myBind(gizmo, 1,2,3);
+console.log(gizmoDoingSomething(4, 5, 6));
