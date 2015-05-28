7119a8af9cd35b6066c5b58b12d1e39895c4ecc5
Loren Peterson <lorenkp@gmail.com>
2015-05-28 15:41:38 -0700

    finished carousel up to durability

--- carousel/carousel.js
+++ carousel/carousel.js
@@ -1,7 +1,51 @@
 $.Carousel = function(el) {
   this.$el = $(el);
   this.activeIdx = 0;
-  this.$el.find('.items::first-child').addClass('active');
-  this.$el.on('click', 'a', this.clickTab.bind(this));
+  $('.items img').eq(this.activeIdx).addClass('active');
 
+  $('.slide-right').on('click', this.slide.bind(this, 1));
+  $('.slide-left').on('click', this.slide.bind(this, -1));
+
+};
+
+$.Carousel.prototype.slide = function(dir) {
+  var act = $('.items img').eq(this.activeIdx);
+  var tempIdx = (this.activeIdx + dir);
+  if (tempIdx < 0) {
+    tempIdx = 2;
+  } else if (tempIdx > 2) {
+    tempIdx = 0;
+  }
+  console.log(tempIdx);
+  var newImg = $('.items img').eq(tempIdx);
+
+  if (dir === 1) {
+    newImg.addClass("right");
+    act.addClass("left");
+  }
+
+  if (dir === -1) {
+    newImg.addClass("left");
+    act.addClass("right");
+  }
+
+  act.one('transitionend', function(event) {
+    act.removeClass('active left right');
+    console.log(event.currentTarget);
+    // act.removeClass('left right');
+  });
+
+  window.setTimeout(function() {
+    newImg.removeClass('left right');
+    newImg.addClass('active');
+  }, 0);
+
+  this.activeIdx = tempIdx;
+};
+
+
+$.fn.carousel = function() {
+  return this.each(function() {
+    new $.Carousel(this);
+  });
 };
