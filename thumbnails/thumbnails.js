44466777266b6e317a615861111a2543db0f05cb
Loren Peterson <lorenkp@gmail.com>
2015-05-28 18:06:55 -0700

    almost done with thumbnails

--- thumbnails/thumbnails.js
+++ thumbnails/thumbnails.js
@@ -1,10 +1,41 @@
 $.Thumbnails = function(el) {
   this.$el = $(el);
-  this.$contentTabs = $(this.$el.attr('data-content-tabs'));
-  this.$activeLink = this.$el.find('.active');
-  this.$active = this.$contentTabs.find('.active');
-  this.$el.on('click', 'a', this.clickTab.bind(this));
+  this.gutterIdx = 1;
+  this.$images = this.$el.find("div.gutter-images img");
+  this.fillGutterImages();
+  this.$gutterImages = this.$el.find("div.gutter-images");
+  this.$activeImg = this.$gutterImages.find('img').eq(0);
+  this.activate(this.$activeImg);
+  var that = this;
+  $('div.gutter-images img').on('click', function(event) {
+    var target = $(event.currentTarget);
+    that.$activeImg = target;
+    that.activate(that.$activeImg);
+  });
 
+  $('div.gutter-images img').on('mouseenter', function(event) {
+    var target = $(event.currentTarget);
+    that.activate(target);
+  });
+
+  $('div.gutter-images img').on('mouseleave', function(event) {
+    that.activate(that.$activeImg);
+  });
+  $('a.nav').on('click', this.fillGutterImages.bind(this));
+};
+
+$.Thumbnails.prototype.activate = function($img) {
+  $('.active').empty();
+  $img.clone().appendTo($('.active'));
+};
+
+$.Thumbnails.prototype.fillGutterImages = function() {
+  this.gutterIdx += 1;
+  $('div.gutter-images').empty();
+  for (var i = 0; i < 5; i++) {
+    this.$images.eq(this.gutterIdx + i).appendTo($('div.gutter-images'));
+  }
+  this.$gutterImages = this.$el.find("div.gutter-images");
 };
 
 $.fn.thumbnails = function() {
